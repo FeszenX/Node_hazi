f3b39eb56ad3db2c2402c5adfdc58aac996a8f01
feszenx <lipcseiadam@sch.bme.hu>
2017-03-19 21:22:10 +0100

    user registration

---
+++ Middleware/checkUserRegistration.js
@@ -0,0 +1,39 @@
+var requireOption = require('../common').requireOption;
+
+module.exports = function (objectrepository) {
+
+    var UserModel = requireOption(objectrepository, "userModel");
+
+    return function (req, res, next) {
+
+        if ((typeof req.body === 'undefined') || (typeof req.body.email === 'undefined') || req.body.password === 'undefined') {
+            return next();
+        }
+        
+        UserModel.findOne({
+            email: req.body.email
+        }, function (err, result) {
+            
+            if ((err) || (result !== null)) {
+                res.tpl.error.push('Your email address is already registered');
+                return next();
+            }
+            
+            if (req.body.name.length < 3) {
+                res.tpl.error.push('The username should be at least 3 ');
+                return next();
+            }
+
+            var newUser = new UserModel();
+            newUser.name = req.body.name;
+            newUser.email = req.body.email;
+            newUser.password = req.body.password;
+            newUser.save(function (err) {
+                return res.redirect('/login');
+            })
+            
+        });
+
+    };
+
+};
